16d41e0572cf3aa4cf49b32625647516f04f349f
Naga Chaitanya Konada <13428505+ChaituKNag@users.noreply.github.com>
2022-03-12 13:56:08 +0000

    final result

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let Parser = require('rss-parser');
-let parser = new Parser();
+let fetch = require('isomorphic-fetch');
+let parseString = require('xml2js').parseString;
 
 (async function() {
     const urls = process.argv.slice(2);
@@ -7,17 +7,37 @@ let parser = new Parser();
     if(urls.length === 0) return;
 
     urls.forEach(async (url, idx) => {
-        let feed = await parser.parseURL(url);
 
-        console.log(`
----------------------------------
-RSS feed for URL: ${url}
-Title: ${feed.title}
-Description: ${feed.description}`);
+    fetch(url)
+        .then(res => res.text())
+        .then(data => {
+            parseString(data, (err, jsonData) => {
+                if(err) {
+                    console.log('There was an error');
+                }
 
-        if(idx === urls.length - 1) {
-            console.log('---------------------------------');
-        }
+
+                let title, description;
+
+                if(jsonData.rss) {
+                    title = jsonData.rss?.channel[0].title[0];
+                    description = jsonData.rss.channel[0].description[0]; 
+                } else if(jsonData.feed) {
+                    title = jsonData.feed.title[0];
+                    description = jsonData.feed.description?.[0] || '';
+                }
+
+                console.log('---------------------------------');
+                console.log(`RSS feed for URL: ${url}`);
+                console.log(`Title: ${title}`);
+                console.log(`Description: ${description}`);
+                
+                
+                if(idx === urls.length - 1) {
+                    console.log('---------------------------------');
+                }
+            });
+        })
     });
     
 })();
